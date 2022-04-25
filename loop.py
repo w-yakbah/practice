a695c30023dd1433bab3c73cf4fcf7683a867e03
w-yakbah <w.yakbah@outlook.com>
2022-04-25 13:21:38 +0200

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,6 @@
+loop = 1
+while (loop < 10):
+    mood = input("How do you feel? ")
+    print ("You are",mood)
+    discuss = input("Do you want to talk about it?")
+    print ("It doesn't matter if you want to discuss it or not, wether you are",mood,"or not, it doesn't change the fact that I am a program.")
